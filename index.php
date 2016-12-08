0133c1183c7061fd603d49241c1dfa1f8e76de19
Yohan <Yohan@192.168.1.42>
2016-12-08 05:32:29 +0100

    Page d'acceuil

--- index.php
+++ index.php
@@ -1,17 +1,150 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+
+<!DOCTYPE HTML>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
+
+<head>
+        <title>Titre principal de la page</title>
+        <meta charset="utf-8">
+        <meta name="description" content="165c. uniques">
+        <link rel="stylesheet" href="assets/css/main.css" />
+</head>
+
+<body>
+        <div id="header">
+            <header>
+                <span class="logo">
+                    <img src="assets/images/logo.png" alt>
+                </span>
+                <h1 id="h1_header">
+                    Rorqual 
+                </h1>
+                <p id="p_header">
+                    Un site de l'université de Pau et des Pays de l'Adour
+                </p>
+            </header>
+        </div>  
+        
+        <div id="main">
+            <div id="introduction">
+                <div id="box_introduction">
+                    <div id="content_introduction">
+                        <div id="header_introduction">
+                            <h2>
+                                Présentation
+                            </h2>
+                        </div>
+                        <div id="barre_introduction">
+                        </div>
+                        <div id="description_introduction">
+                            <p>
+                                Sed lorem ipsum dolor sit amet nullam consequat feugiat consequat magna adipiscing magna etiam amet veroeros. Lorem ipsum dolor tempus sit cursus. Tempus nisl et nullam lorem ipsum dolor sit amet aliquam.
+                            </p>
+                        </div>
+                        
+                        <div id="button_introduction">
+                            Présentation
+                        </div>
+                    </div>
+                    <div id="img_introduction">
+                        <img src="assets/images/présentation.png" alt>
+                    </div>
+                </div>
+            </div>          
+            
+            
+            <div id="fonction">
+                <div id="box_fonction">
+                    <div id="content_fonction">
+                        <div id="header_fonction">
+                            <h2>
+                                Fonctionnalités
+                            </h2>
+                        </div>
+                        
+                        <div id="barre_fonction">
+                        </div>
+                        
+                        <div class="child">
+                            <img src="assets/images/testimony.png" alt>
+                            <h3>
+                                Témoignage
+                            </h3>
+                            <p>
+                                Sed lorem amet ipsum dolor et amet nullam consequat a feugiat consequat tempus veroeros sed consequat.
+                            </p>
+                            
+                        </div>
+                        <div class="child">
+                            <img src="assets/images/graphic.png" alt>
+                            <h3>
+                                Compétences
+                            </h3>
+                            <p>
+                                Sed lorem amet ipsum dolor et amet nullam consequat a feugiat consequat tempus veroeros sed consequat.
+                            </p>
+                            
+                        </div>
+                        <div class="child">
+                            <img src="assets/images/consultation.png" alt>
+                            <h3>
+                                Consultation
+                            </h3>
+                            <p>
+                                Sed lorem amet ipsum dolor et amet nullam consequat a feugiat consequat tempus veroeros sed consequat.
+                            </p>
+                            
+                        </div>
+                        
+                        
+                    </div>
+                </div>
+            </div> 
+        </div>        
+         
+         
+    
+        <nav><!-- Nav. principale de la page -> site --></nav> 
+        <aside><!-- Les à-cotés de la page --></aside>
+        <article></article>
+        <footer>
+            <div id="footer">
+                <div id="footer_left">
+                    <h2>
+                        Aliquam sed mauris
+                    </h2>
+                    <p id="p_footer">
+                        Sed lorem ipsum dolor sit amet et nullam consequat feugiat consequat magna adipiscing tempus etiam dolore veroeros. eget dapibus mauris. Cras aliquet, nisl ut viverra sollicitudin, ligula erat egestas velit, vitae tincidunt odio.
+                    </p>
+                    <div id="button_footer">
+                            Plus d'info
+                    </div>
+                </div>   
+                
+                <div id="footer_right">
+                    <h2>Contact</h2>
+                    <dl class="alt">
+                        <dt>Adresse</dt> 
+                        <dd>Avenue de l'Université • 64012 • Pau</dd>
+                        <dt>Téléphone</dt>
+                        <dd>05 59 40 70 00</dd>
+                        <dt>Email</dt>
+                        <dd>contact@uppa.fr</dd>                       
+                    </dl>
+                    
+                </div> 
+                
+            </div>
+            
+        </footer>   
+</body>
 </html>
+
